e6e01232fa9b8f87a94e2d2f89f82874dc8285db
Jerblyka <yanbeda94@gmail.com>
2025-02-11 03:32:59 -0300

    Update Novo-pos-Install-setup.sh

--- Novo-pos-Install-setup.sh
+++ Novo-pos-Install-setup.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
 # Atualizando os repositórios e o sistema
-sudo apt update -y && sudo apt upgrade -y
+sudo apt update && sudo apt full-upgrade -y
 
 # Instalando os pacotes
-sudo apt install -y ksnip vlc psensor gnome-system-monitor default-jre bluedevil streamlink mpv ffmpeg chrony htop
+sudo apt install -y locales console-common ksnip vlc psensor gnome-system-monitor default-jre bluedevil streamlink mpv ffmpeg chrony htop qbittorrent
 
 # Configurando o sistema para PT-BR
-#sudo apt install -y locales
-#sudo locale-gen pt_BR.UTF-8
-#sudo update-locale LANG=pt_BR.UTF-8
+sudo locale-gen pt_BR.UTF-8
+sudo update-locale LANG=pt_BR.UTF-8
 
 # Atualizando o relógio para o horário de São Paulo
 sudo timedatectl set-timezone America/Sao_Paulo
 
 # Configurando o teclado para ABNT2
-#sudo apt install -y console-common
-#sudo loadkeys br-abnt2
-#echo 'XKBLAYOUT="br"' | sudo tee -a /etc/default/keyboard
-#echo 'XKBVARIANT="abnt2"' | sudo tee -a /etc/default/keyboard
-#sudo dpkg-reconfigure --frontend=noninteractive keyboard-configuration
-
-# Reiniciando para aplicar as mudanças
-#sudo reboot
+sudo loadkeys br-abnt2
+echo 'XKBLAYOUT="br"' | sudo tee -a /etc/default/keyboard
+echo 'XKBVARIANT="abnt2"' | sudo tee -a /etc/default/keyboard
+sudo dpkg-reconfigure --frontend=noninteractive keyboard-configuration
